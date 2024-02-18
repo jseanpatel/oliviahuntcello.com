36f2aff8ee71e3a7261ec0ca30465fe2db9e8a0c
Jacob Patel <jseanpatel@gmail.com>
2024-02-18 15:51:11 -0800

    chore: begin port from ts to js

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,6 +8,8 @@ import {
   InstagramIcon,
   LinkedInIcon,
   XIcon,
+  TwitterIcon,
+
 } from '@/components/SocialIcons'
 import portraitImage from '@/images/portrait.jpg'
 
@@ -16,9 +18,9 @@ function SocialLink({ className, href, children, icon: Icon }) {
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+        className="flex text-sm font-medium transition group text-zinc-800 hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
       >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <Icon className="flex-none w-6 h-6 transition fill-zinc-500 group-hover:fill-teal-500" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
@@ -52,64 +54,65 @@ export default function About() {
               src={portraitImage}
               alt=""
               sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              className="object-cover border-2 border-black aspect-square rotate-3 rounded-2xl bg-zinc-100 dark:bg-zinc-800 dark:border-white dark:border-zinc-700 "
             />
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
-            future.
+            Motion through music
           </h1>
-          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+          <div className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              Cellist Olivia Hunt grew up in Milton, Georgia where she began
+              learning the cello in school at age 9. She attended Vanderbilt
+              University’s Blair School of Music, where she earned her
+              Bachelor’s in Music under the instruction of Dr. Felix Wang.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              While an undergraduate, she performed with the Nashville
+              Sinfonietta, spent a semester studying music at the Conservatorium
+              van Amsterdam, and performed as part of the Festival d’Aix en
+              Provence with her string quartet.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              Following graduation, she continued her studies at Indiana
+              University with Eric Kim, where she earned her Master’s in Music
+              and Performer’s Diploma in Solo Cello Performance. She has
+              attended festivals such as the Aspen Music Festival, Green
+              Mountain Chamber Music Festival, Brancaleoni International Music
+              Festival, and National Music Festival.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              She is currently the Assistant Principal Cellist of the Billings
+              Symphony and acts as a substitute in the Owensboro Symphony
+              Orchestra and Evansville Philharmonic. In addition to performing
+              she has a passion for teaching, and is on faculty at the Stafford
+              Music Academy in Bloomington, Indiana. Outside of music she enjoys
+              traveling, cuddling her cat, and reading.
             </p>
           </div>
         </div>
         <div className="lg:pl-20">
           <ul role="list">
-            <SocialLink href="#" icon={XIcon}>
-              Follow on X
+            <SocialLink href="#" icon={TwitterIcon}>
+              Follow on Twitter
             </SocialLink>
             <SocialLink href="#" icon={InstagramIcon} className="mt-4">
               Follow on Instagram
             </SocialLink>
-            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+            {/* <SocialLink href="#" icon={GitHubIcon} className="mt-4">
               Follow on GitHub
-            </SocialLink>
+            </SocialLink> */}
             <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="olivia.c.hunt@gmail.com"
               icon={MailIcon}
-              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+              className="pt-8 mt-8 border-t border-zinc-100 dark:border-zinc-700/40"
             >
-              spencer@planetaria.tech
+              olivia.c.hunt@gmail.com
             </SocialLink>
           </ul>
         </div>
